5598a08b6ca4b3ba5d60aa0b4285df61e5a98667
Franklin Kong <franklinkong981@gmail.com>
2024-11-26 12:58:31 -0800

    Added JSONSchema

--- routes/books.js
+++ routes/books.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const router = new express.Router();
 const ExpressError = require("../expressError");
+const jsonschema = require("jsonschema");
+const bookSchema = require("../schemas/bookSchema.json");
 
-//VERSION WITHOUT ANY REAL VALIDATION...
-router.post("/", function (req, res, next) {
-   const bookData = req.body.book;
+router.post("/", function(req, res, next) {
+  const result = jsonschema.validate(req.body, bookSchema);
+  if (!result.valid) {
+    const listOfErrors = result.errors.map(e => e.stack);
+    const err = new ExpressError(listOfErrors, 400);
 
-   if (!bookData) {
-     // pass a 400 error to the error-handler
-     let error = new ExpressError("Book data is required", 400);
-     return next(error);
-   }
-
-   // (not implemented) insert book into database here
-
-   return res.json(bookData);
- });
+    return next(err);
+  } else {
+    const {book} = req.body.book;
+    return res.json({book});
+  }
+});
 
 module.exports = router;
